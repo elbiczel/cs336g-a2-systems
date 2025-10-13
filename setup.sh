001046a92111323f80f03325ad0c7b48354cfeb0
Tomasz Biczel <tomasz@biczel.com>
2025-10-13 20:33:24 +0000

    Update set up for manual nsys-repo exports.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
+# Usage: source setup.sh
+
 git config --global user.email "tomasz@biczel.com"
 git config --global user.name "Tomasz Biczel"
 
 sudo apt-get update
 sudo apt-get install nsight-systems
 
-ln -s /workspace/data/a2 data
+[[ ! -e data ]] && ln -s /workspace/data/a2/ data
+
+# To manually run QdstrmImporter on profiles.
+export PATH=/usr/lib/nsight-systems/host-linux-x64:$PATH
 
 # To install dependencies
 uv run python
