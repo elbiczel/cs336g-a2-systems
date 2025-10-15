39bcd7e16a0cc42cd791f83e25707d3596555ed6
Tomasz Biczel <tomasz@biczel.com>
2025-10-15 23:35:12 +0200

    Sync device before measuring post optimizer memory

--- cs336_systems/run_benchmark.py
+++ cs336_systems/run_benchmark.py
@@ -155,18 +155,21 @@ def _benchmark(cfg, transformer_cfg, context_length) -> dict[str, Any]:
         utils.synchronize(cfg.device)
         allocated = torch.cuda.memory_allocated() / 1024**2  # in MB
         loss = model(xb).sum()
+        utils.synchronize(cfg.device)
         fwd_allocated = torch.cuda.memory_allocated() / 1024**2  # in MB
         if opt is not None:
             opt.zero_grad()
         loss.backward()
         utils.synchronize(cfg.device)
         back_allocated = torch.cuda.memory_allocated() / 1024**2  # in MB
-        utils.synchronize(cfg.device)
         if opt is not None:
             opt.step()
+            utils.synchronize(cfg.device)
             opt_allocated = torch.cuda.memory_allocated() / 1024**2  # in MB
+            utils.synchronize(cfg.device)
         else:
             opt_allocated = np.nan
+            utils.synchronize(cfg.device)
 
     def fwd():
         with nvtx.range("fwd"):
