694a3569c002dee9ea5dfb15b2edfee5c9d63df2
Tomasz Biczel <tomasz@biczel.com>
2025-11-20 22:37:22 +0100

    Torch flash attn implementation

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -1,17 +1,76 @@
 """Implementation of a FlashAttention2 kernel."""
 
-from torch import autograd
+import math
+import torch
+from torch import autograd, Tensor
+from jaxtyping import Float
+from einops import einsum
 
 
-class FlashAttentionFunc(autograd.Function):
+def torch_flash_attn(
+    Q: Float[Tensor, "... n_q d_k"],
+    K: Float[Tensor, "... n_k d_k"],
+    V: Float[Tensor, "... n_k d_v"],
+    is_causal: bool,
+    Q_TILE_SIZE,
+    K_TILE_SIZE,
+) -> Float[Tensor, " n_q d_v"]:
+    d_k, d_v, l_dims = Q.shape[-1], V.shape[-1], Q.shape[:-1]
+    out_dims = l_dims + (d_v,)
+    O = torch.empty(out_dims, device=Q.device, dtype=Q.dtype)
+    L = torch.empty(l_dims, device=Q.device, dtype=Q.dtype)
+
+    T_q = Q.shape[-2] // Q_TILE_SIZE
+    T_k = K.shape[-2] // K_TILE_SIZE
+    for i in range(T_q):
+        Q_i = Q[..., i * Q_TILE_SIZE : (i + 1) * Q_TILE_SIZE, :]
+        M_i = torch.full(
+            Q_i.shape[:-1], float("-inf"), device=Q_i.device, dtype=Q_i.dtype
+        )
+        L_i = torch.zeros_like(M_i)
+        O_i = torch.zeros(
+            *Q_i.shape[:-1], d_v, device=Q_i.device, dtype=Q_i.dtype
+        )
+        for j in range(T_k):
+            K_j = K[..., j * K_TILE_SIZE : (j + 1) * K_TILE_SIZE, :]
+            V_j = V[..., j * K_TILE_SIZE : (j + 1) * K_TILE_SIZE, :]
+            S_ij = einsum(
+                Q_i, K_j, "... b_q d_k, ... b_k d_k -> ... b_q b_k"
+            ) / math.sqrt(d_k)
+            prev_M_i = M_i
+            M_i = torch.maximum(M_i, S_ij.max(dim=-1).values)
+            P_ij = torch.exp(S_ij - M_i.unsqueeze(-1))
+            alpha = torch.exp(prev_M_i - M_i)
+            L_i = alpha * L_i + P_ij.sum(dim=-1)
+            O_i = alpha.unsqueeze(-1) * O_i + einsum(
+                P_ij, V_j, "... b_q b_k, ... b_k d_v -> ... b_q d_v"
+            )
+        O[..., i * Q_TILE_SIZE : (i + 1) * Q_TILE_SIZE, :] = (
+            O_i / L_i.unsqueeze(-1)
+        )
+        L[..., i * Q_TILE_SIZE : (i + 1) * Q_TILE_SIZE] = M_i + torch.log(L_i)
+    return O, L
+
+
+class TorchFlashAttentionFunc(autograd.Function):
     @staticmethod
-    def forward(ctx):
-        # Note: You will need to update the signature of this method
-        raise NotImplementedError("TODO: Implement FlashAttentionFunc.forward")
+    def forward(
+        ctx,
+        Q: Float[Tensor, "n_q d"],
+        K: Float[Tensor, "n_k d"],
+        V: Float[Tensor, "n_k d"],
+        is_causal: bool = False,
+    ):
+        # TODO: Tune the tile sizes.
+        ctx.Q_TILE_SIZE = 16
+        ctx.K_TILE_SIZE = 16
+        O, L = torch_flash_attn(
+            Q, K, V, is_causal, ctx.Q_TILE_SIZE, ctx.K_TILE_SIZE
+        )
+        ctx.save_for_backward(Q, K, V, L)
+        return O
 
     @staticmethod
     def backward(ctx):
         # Note: You will need to update the signature of this method
-        raise NotImplementedError(
-            "TODO: Implement FlashAttentionFunc.backward"
-        )
+        raise NotImplementedError("TODO: Implement FlashAttentionFunc.backward")
--- tests/adapters.py
+++ tests/adapters.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Type
+from cs336_systems import flash_attention
 
 import torch
 
@@ -15,8 +16,7 @@ def get_flashattention_autograd_function_pytorch() -> Type:
     Returns:
         A class object (not an instance of the class)
     """
-    # For example: return MyFlashAttnAutogradFunctionClass
-    raise NotImplementedError
+    return flash_attention.TorchFlashAttentionFunc
 
 
 def get_flashattention_autograd_function_triton() -> Type:
